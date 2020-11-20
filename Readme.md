093bd12d42a6a7133119a6201e323705891dd002
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2020-11-20 10:10:31 -0600

    Add end point for creating a user. Add documentation on how to use the end points

--- Readme.md
+++ Readme.md
@@ -2,8 +2,27 @@
 
 This repo is meant to be a play ground on creating a custom login and jwt authentication system with python. 
 
-# Run instructions
+## Run instructions
 
 `export FLASKAPP=app.py`
 
 `python3 -m flask run`
+
+
+## Usages
+
+**Create User**
+
+```bash
+curl --header "Content-Type: application/json" --request POST --data '{"username":"<username>","password":"<password>","email":"<emailaddress>"}' http://<flaskid>:<port>/register
+
+```
+
+** Login, get a token **
+
+```bash
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"username":"<username>","password":"<password>"}' \
+  http://<flaskid>:<port>/login
+```
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request
 import json
 import requests
-from users import userExist, PasswordMatchesForUser,User
+from users import userExist, PasswordMatchesForUser,User,CreateUser
 from jwtoken import CreateTokenForUser,GetTokenFromTokenId
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def home():
     return "<h1 style='text-align: center;'>Welcome to the User Service.</h1>"
 
 @app.route('/login', methods=['POST']) #GET requests will be blocked
-def json_example():
+def login():
     req_data = request.get_json()
     print(req_data)
     password = req_data['password']
@@ -29,6 +29,25 @@ def json_example():
     tokenid = CreateTokenForUser(usr)
     tokenString = GetTokenFromTokenId(tokenid)
     response = {"success":True,"message":"success","token":tokenString}
+    return json.dumps(response)
+
+@app.route('/register', methods=['POST']) #GET requests will be blocked
+def register():
+    req_data = request.get_json()
+    response = {"success":False,"message":"username already in use"}
+    password = req_data['password']
+    username = req_data['username']
+    email = req_data['email']
+
+    if userExist(username):
+        return json.dumps(response)
+
+    success = CreateUser(username,password,email)
+
+    if success:
+        response = {"success":True,"message":"User Created Successfully"}
+    else:
+        response = {"success":False,"message":"Error Creating User"}
     return json.dumps(response)
 
     

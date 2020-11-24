2bb15174bfae0addbb2e448ee1a42d64868ba83e
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2020-11-24 15:40:48 +0000

    test commiting with mounted .ssh folder

--- app/main.py
+++ app/main.py
@@ -88,8 +88,3 @@ async def GetUser(token: str = ""):
     else:
         returnVal = {"username":usr.username,"email":usr.email,"id":usr.id,"success":True}
         return returnVal
-#
-    
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=5000)
